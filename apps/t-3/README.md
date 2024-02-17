d8f1d2e93331f8970e2ed3c72188a06b5efc3441
Zain Fathoni <me@zainf.dev>
2024-02-17 14:53:17 +0800

    docs(t-3): document assumptions

---
+++ apps/t-3/README.md
@@ -0,0 +1,15 @@
+# Task 3
+
+## Assumptions
+
+Assumed function signatures:
+
+```js
+convertGrossSalesToCurrency("2024-01", conversionRates, 29184); //=> $4,812.9
+convertGrossSalesToCurrency("2024-01", conversionRates, 38009); //=> $301.89
+convertGrossSalesToCurrency("2024-01", conversionRates, 40821); //=> $53,776.40
+
+formatLastPurchasedAt(29184); //=> null
+formatLastPurchasedAt(38009); //=> 11/14/2023, 6:51:49 AM
+formatLastPurchasedAt(40821); //=> 1/29/2024, 11:21:56 PM
+```
--- apps/t-3/src/index.js
+++ apps/t-3/src/index.js
@@ -98,11 +98,3 @@ export function formatLastPurchasedAt(productId) {
       })
     : null;
 }
-
-// convertGrossSalesToCurrency("2024-01", conversionRates, 29184); //=> $4,812.9
-// convertGrossSalesToCurrency("2024-01", conversionRates, 38009); //=> $301.89
-// convertGrossSalesToCurrency("2024-01", conversionRates, 40821); //=> $53,776.40
-
-// formatLastPurchasedAt(29184); //=> null
-// formatLastPurchasedAt(38009); //=> 11/14/2023, 6:51:49 AM
-// formatLastPurchasedAt(40821); //=> 1/29/2024, 11:21:56 PM
